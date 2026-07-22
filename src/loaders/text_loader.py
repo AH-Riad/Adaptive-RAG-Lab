c66a3b3f843bb365f2fc0526196222c839915fbc
Riad <riadk910@gmail.com>
2026-07-22 23:03:03 +0600

    Text loader

--- src/loaders/text_loader.py
+++ src/loaders/text_loader.py
@@ -1,3 +1,20 @@
-loader = TXTLoader()
+from pathlib import Path
 
-documents = loader.load(path)
+from src.core import Document
+from .base_loader import BaseLoader
+
+
+class TextLoader(BaseLoader):
+
+    def load(self, path: str):
+        file_path = Path(path)
+
+        text = file_path.read_text(encoding="utf-8")
+
+        document = Document(
+            id=file_path.stem,
+            source=file_path.name,
+            text=text
+        )
+
+        return [document]
