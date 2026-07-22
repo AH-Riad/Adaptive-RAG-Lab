c8c9824534f62b90ae73e4430dbefae3f936742d
Riad <riadk910@gmail.com>
2026-07-22 23:02:24 +0600

    pdf loader

--- src/loaders/pdf_loader.py
+++ src/loaders/pdf_loader.py
@@ -1,3 +1,29 @@
-loader = PDFLoader()
+from pathlib import Path
+from pypdf import PdfReader
 
-documents = loader.load(path)
+from src.core import Document
+from .base_loader import BaseLoader
+
+
+class PDFLoader(BaseLoader):
+
+    def load(self, path: str):
+        reader = PdfReader(path)
+
+        pages = []
+
+        for page in reader.pages:
+            pages.append(page.extract_text())
+
+        text = "\n".join(pages)
+
+        document = Document(
+            id=Path(path).stem,
+            source=Path(path).name,
+            text=text,
+            metadata={
+                "num_pages": len(reader.pages)
+            }
+        )
+
+        return [document]
