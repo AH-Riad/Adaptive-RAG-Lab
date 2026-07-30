6aa37d3a3b6236cd9379e25802aaa63da16bda32
Riad <riadk910@gmail.com>
2026-07-30 12:09:54 +0600

    sentence_transformer_embeddingy

---
+++ src/embeddings/sentence_transformer_embedding.py
@@ -0,0 +1,42 @@
+from sentence_transformers import SentenceTransformer
+from .base_embedding import BaseEmbedding
+from .embedding_result import EmbeddingResult
+
+class SentenceTransformerEmbedding(BaseEmbedding):
+
+    def __init__(
+        self,
+        model_name="all-MiniLM-L6-v2"
+    ):
+
+        print(f"Loading {model_name}...")
+
+        self.model = SentenceTransformer(model_name)
+
+        self.model_name = model_name
+        
+    def encode(self, chunks):
+
+        texts = [chunk.text for chunk in chunks]
+
+        vectors = self.model.encode(texts)
+
+        results = []
+
+        for chunk, vector in zip(chunks, vectors):
+
+            results.append(
+
+                EmbeddingResult(
+
+                    chunk=chunk,
+
+                    embedding=vector.tolist(),
+
+                    model_name=self.model_name
+
+                )
+
+            )
+
+        return results
