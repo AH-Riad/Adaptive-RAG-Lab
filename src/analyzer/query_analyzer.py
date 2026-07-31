9d949fdcde2adf2399f2185a58d22a26ac28727d
Riad <riadk910@gmail.com>
2026-07-31 18:12:53 +0600

    Query Analyzer

---
+++ src/analyzer/query_analyzer.py
@@ -0,0 +1,19 @@
+class QueryAnalyzer:
+
+    COMPARISON_WORDS = {
+        "compare",
+        "difference",
+        "versus",
+        "vs",
+        "better"
+    }
+
+    QUESTION_WORDS = {
+        "what",
+        "why",
+        "how",
+        "when",
+        "where",
+        "who",
+        "which"
+    }
