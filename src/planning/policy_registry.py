2b3323dc03e4972a15e7bdcd955385d8d32c4278
Riad <riadk910@gmail.com>
2026-08-08 15:12:56 +0600

    Fix Import Issue

--- src/planning/policy_registry.py
+++ src/planning/policy_registry.py
@@ -1,4 +1,4 @@
-from planning.policies.confidence_policy import ConfidencePolicy
+from src.planning.policies.confidence_policy import ConfidencePolicy
 from src.planning.policies.chunk_policy import ChunkPolicy
 from src.planning.policies.retrieval_policy import RetrievalPolicy
 from src.planning.policies.topk_policy import TopKPolicy
